2d88999c26cd1e6aaaeb0a8c2aec465df93ecaf6
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2023-10-16 17:57:52 +0200

    Created first version of website

---
+++ js/translate.js
@@ -0,0 +1,9 @@
+const textTranslations = ["Strona dostępna wkrótce!", "Mój profil GitHub"];
+
+if (window.navigator.language.split("-")[0] == "pl") {
+    document.querySelector("html").lang = "pl";
+
+    document.querySelectorAll("[text-id]").forEach((e) => {
+        e.innerHTML = textTranslations[e.getAttribute("text-id")];
+    });
+}
